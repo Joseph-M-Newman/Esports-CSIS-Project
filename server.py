286d3c53d1cb26622da1973132510e0fc3965bd6
Joseph Newman <josephnew900@gmail.com>
2021-11-05 00:16:43 -0700

    Update server.py

--- server.py
+++ server.py
@@ -9,23 +9,6 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 @app.route("/")
 def home():
     return flask.render_template("index.html")
-
-@app.route("")
-def rocketleague():
-    return flask.render_template("")
-
-
-def league():
-    return flask.render_template("")
-
-@app.route("")
-def valorant():
-    return flask.render_template("")
-
-@app.route("/test")
-def test():
-    return flask.render_template("")
-
 # === run the server == default port localhost:5000 === (put on heroku to use site live)  
 if __name__ == "__main__":
     app.run(debug=True)
